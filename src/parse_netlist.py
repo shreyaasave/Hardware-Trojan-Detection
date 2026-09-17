02ae9cf330f495dd38946c505c07777d281c2bd1
Shreya Save <shreyaasave@gmail.com>
2026-09-17 11:42:55 +0530

    Restore .gitignore, README, architecture, and dataset folders on saher branch

--- src/parse_netlist.py
+++
@@ -1,9 +0,0 @@
-from pyverilog.vparser.parser import parse
-
-netlist = ["../Dataset/trojan/AES-T100/src/TjFree/aes_clean_netlist.v"]
-
-ast, directives = parse(netlist)
-
-print("Successfully parsed!")
-
-print(type(ast))
